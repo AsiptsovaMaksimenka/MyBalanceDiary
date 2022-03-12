431b54a25cbe93037ff76cb450b872084c2d74c8
Asiptsova <101019340+AsiptsovaMaksimenka@users.noreply.github.com>
2022-03-12 19:35:40 +0100

    Update RegisterActivity.java

--- app/src/main/java/com/application/mybalancediary/RegisterActivity.java
+++ app/src/main/java/com/application/mybalancediary/RegisterActivity.java
@@ -4,16 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -27,14 +26,18 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class RegisterActivity extends AppCompatActivity {
     public static final String TAG = "TAG";
-    EditText mFullName,mEmail,mPassword;
+    EditText mFullName,mEmail,mPassword,mHeight,mWeight,mAge;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
     FirebaseFirestore fStore;
+    RadioGroup genderGroup;
+    int position;
+    String genderMF;
     String userID;
 
     @Override
@@ -50,7 +53,23 @@ public class RegisterActivity extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
-
+        mHeight   = findViewById(R.id.heightInput);
+        mWeight   = findViewById(R.id.weightInput);
+        mAge   = findViewById(R.id.ageInput);
+        genderGroup = findViewById(R.id.genderGroup);
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            position = genderGroup.indexOfChild(findViewById(checkedId));
+            if (position == 0) {
+                Log.d("Gender is ", "Male");
+                genderMF="Male";
+            } else {
+                Log.d("Gender is ", "Female");
+                genderMF="Female";
+            }
+        }
+        });
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
@@ -63,6 +82,26 @@ public class RegisterActivity extends AppCompatActivity {
                 final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
                 final String fullName = mFullName.getText().toString();
+                final String height=mHeight.getText().toString();
+                final String weight=mWeight.getText().toString();
+                final String age=mAge.getText().toString();
+                final String gender=genderMF;
+
+
+                if(TextUtils.isEmpty(age)){
+                    mAge.setError("Age is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(weight)){
+                    mWeight.setError("Weight is Required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(height)){
+                    mHeight.setError("Height is Required.");
+                    return;
+                }
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is Required.");
@@ -107,6 +146,10 @@ public class RegisterActivity extends AppCompatActivity {
                             Map<String,Object> user = new HashMap<>();
                             user.put("fName",fullName);
                             user.put("email",email);
+                            user.put("weight",weight);
+                            user.put("height",height);
+                            user.put("age",age);
+                            user.put("gender",gender);
                             documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
@@ -137,6 +180,7 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),LoginActivity.class));
             }
         });
+
 
     }
 }
