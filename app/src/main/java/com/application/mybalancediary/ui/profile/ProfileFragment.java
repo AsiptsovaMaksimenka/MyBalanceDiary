233b61d62cce0e636ee1284d4e133e67ab61cd4c
Asiptsova <asipcova@gmail.com>
2022-04-20 19:52:24 +0200

    <versionNew change profile picture>

--- app/src/main/java/com/application/mybalancediary/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/application/mybalancediary/ui/profile/ProfileFragment.java
@@ -1,14 +1,23 @@
 package com.application.mybalancediary.ui.profile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,15 +30,24 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import com.application.mybalancediary.R;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 
 public class ProfileFragment extends Fragment {
+
+    private interface PictuteListener {
+        void onProfilePictureUpdated();
+    }
 
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
     StorageReference storageReference;
+    private PictuteListener pictureListener;
+    private final int PICK_IMAGE = 100;
+    ImageView image;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,14 +61,32 @@ public class ProfileFragment extends Fragment {
         final TextView calories = root.findViewById(R.id.calories);
         final TextView edit_goal = root.findViewById(R.id.goals);
         final TextView edit_workout = root.findViewById(R.id.Workout);
-
-
+        image =root.findViewById(R.id.profile_picture);
 
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Users");
         storageReference = FirebaseStorage.getInstance().getReference();
+StorageReference profRef=storageReference.child("Users/"+firebaseAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                image.setBackground(null);
+                Picasso.get().load(uri).into(image);
+
+            }
+        });
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGallery=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery,1000);
+            }
+        });
+
+
         Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
         query.addValueEventListener(new ValueEventListener() {
             @Override
@@ -85,6 +121,38 @@ public class ProfileFragment extends Fragment {
 
 
         return root;
+    }
+    @Override
+   public void  onActivityResult(int requestCode,int resultCode,@androidx.annotation.Nullable Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1000)
+        {
+            Uri imageUri=data.getData();
+           // image.setImageURI(imageUri);
+            uploadImageToFirebase(imageUri);
+        }
+    }
+    public void uploadImageToFirebase (Uri imageUri)
+    {
+     StorageReference fileRef=storageReference.child("Users/"+firebaseAuth.getCurrentUser().getUid()+"/profile.jpg");
+     fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+         @Override
+         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+             fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                 @Override
+                 public void onSuccess(Uri uri) {
+                     Picasso.get().load(uri).into(image);
+                 }
+             });
+         }
+     }).addOnFailureListener(new OnFailureListener() {
+         @Override
+         public void onFailure(@NonNull Exception e) {
+
+             Toast.makeText(getActivity(),"Failed",Toast.LENGTH_SHORT).show();
+         }
+     });
     }
 }
 
